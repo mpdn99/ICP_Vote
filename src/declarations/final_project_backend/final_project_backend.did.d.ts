ecf6e15b3e477feb846a166eb045281dd040ad2e
nghiapd2 <nghiapd@solashi.com>
2023-12-18 17:23:34 +0700

    init

---
+++ src/declarations/final_project_backend/final_project_backend.did.d.ts
@@ -0,0 +1,33 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export type Choice = { 'Approve' : null } |
+  { 'Pass' : null } |
+  { 'Reject' : null };
+export interface CreateProposal {
+  'description' : string,
+  'is_active' : boolean,
+}
+export interface Proposal {
+  'reject' : number,
+  'owner' : Principal,
+  'voted' : Array<Principal>,
+  'pass' : number,
+  'approve' : number,
+  'description' : string,
+  'is_active' : boolean,
+}
+export type Result = { 'Ok' : null } |
+  { 'Err' : VoteError };
+export type VoteError = { 'AlreadyVoted' : null } |
+  { 'ProposalIsNotActive' : null } |
+  { 'AccessRejected' : null } |
+  { 'NoSuchProposal' : null };
+export interface _SERVICE {
+  'create_proposal' : ActorMethod<[bigint, CreateProposal], [] | [Proposal]>,
+  'edit_proposal' : ActorMethod<[bigint, CreateProposal], Result>,
+  'end_proposal' : ActorMethod<[bigint], Result>,
+  'get_proposal' : ActorMethod<[bigint], [] | [Proposal]>,
+  'get_proposal_count' : ActorMethod<[], bigint>,
+  'vote' : ActorMethod<[bigint, Choice], Result>,
+}
